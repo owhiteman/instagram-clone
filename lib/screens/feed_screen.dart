99947a05a1dac81777c9d136db3f967bb6006015
owhiteman <54307171+owhiteman@users.noreply.github.com>
2022-08-24 08:28:12 +1000

    Home feed UI

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/post_card.dart';
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.messenger_outline),
+          ),
+        ],
+      ),
+      body: const PostCard(),
+    );
+  }
+}
